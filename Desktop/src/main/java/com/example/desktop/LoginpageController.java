f93dc4d7aee0e999c11c54c01431edc34830e3a6
Safwan Salehin <salehin2207044@stud.kuet.ac.bd>
2025-12-10 05:53:06 +0600

    Admin Dashboard
    Dashboard page well built with style.

--- Desktop/src/main/java/com/example/desktop/LoginpageController.java
+++ Desktop/src/main/java/com/example/desktop/LoginpageController.java
@@ -6,6 +6,9 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import kotlin.io.OnErrorAction;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -14,8 +17,17 @@ import java.sql.ResultSet;
 public class LoginpageController {
     @FXML private TextField username;
     @FXML private PasswordField password;
-    @FXML private Button button, returnbtn;
 
+    private void go(ActionEvent event, String fxml, Integer width, Integer height, String message) {
+        try {
+            javafx.scene.Parent root = javafx.fxml.FXMLLoader.load(getClass().getResource(fxml));
+            javafx.stage.Stage stage = (javafx.stage.Stage)((javafx.scene.Node)event.getSource()).getScene().getWindow();
+            stage.setScene(new javafx.scene.Scene(root, width,height));
+            stage.setTitle(message);
+            stage.show();
+        }
+        catch (Exception e) {e.printStackTrace();}
+    }
 
     @FXML
     void Verify(ActionEvent event) {
@@ -30,12 +42,13 @@ public class LoginpageController {
 
         if (verifyLogin(user, pass)) {
             showAlert(Alert.AlertType.INFORMATION, "Login Successful", "Welcome, " + user + "!");
-            // ekhane eta kora lagbe go(event, "admindashboard.fxml");
+            go(event, "admindashboard.fxml",1300,900,"Admin Dashboard");
         } else {
             showAlert(Alert.AlertType.ERROR, "Login Failed", "Invalid username or password");
             password.clear();
         }
     }
+
 
     private boolean verifyLogin(String username, String password) {
         String query = "SELECT * FROM admin WHERE username = ? AND password = ?";
@@ -69,17 +82,8 @@ public class LoginpageController {
         alert.showAndWait();
     }
 
-    private void go(ActionEvent event, String fxml) {
-        try {
-            javafx.scene.Parent root = javafx.fxml.FXMLLoader.load(getClass().getResource(fxml));
-            javafx.stage.Stage stage = (javafx.stage.Stage)((javafx.scene.Node)event.getSource()).getScene().getWindow();
-            stage.setScene(new javafx.scene.Scene(root, 600,450));
-            stage.setTitle("Movies Ticketing System");
-            stage.show();
-        }
-        catch (Exception e) {e.printStackTrace();}
-    }
 
-    @FXML void GoBack (ActionEvent event){go(event, "homepage.fxml");}
+
+    @FXML void GoBack (ActionEvent event){go(event, "homepage.fxml",600,450,"Movies Ticketing System");}
 
 }
